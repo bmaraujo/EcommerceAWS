8c7c29cbe33a2f65fa1f8dece6ea524542eb5eb8
Bruno Araújo <8039621+bmaraujo@users.noreply.github.com>
2025-11-25 14:03:24 -0300

    Added Lambda Insigths

--- lib/productsApp-stack.ts
+++ lib/productsApp-stack.ts
@@ -47,7 +47,8 @@ export class ProductsAppStack extends cdk.Stack {
                 PRODUCTS_DDB: this.productsDdb.tableName
             },
             layers: [productsLayer],
-            tracing: lambda.Tracing.ACTIVE
+            tracing: lambda.Tracing.ACTIVE,
+            insightsVersion: lambda.LambdaInsightsVersion.VERSION_1_0_404_0
         });
         this.productsDdb.grantReadData(this.productFetchHanlder);
 
@@ -67,7 +68,8 @@ export class ProductsAppStack extends cdk.Stack {
                 PRODUCTS_DDB: this.productsDdb.tableName
             },
             layers: [productsLayer],
-            tracing: lambda.Tracing.ACTIVE
+            tracing: lambda.Tracing.ACTIVE,
+            insightsVersion: lambda.LambdaInsightsVersion.VERSION_1_0_404_0
         });
         this.productsDdb.grantWriteData(this.productAdminHanlder);
     }
